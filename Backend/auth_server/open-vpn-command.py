5f418434439391916735449ff746beb7c0ca2dad
KeAndre96 <grahamkeandre@gmail.com>
2020-04-26 14:18:08 -0400

    updated terminal script

--- Backend/auth_server/open-vpn-command.py
+++ Backend/auth_server/open-vpn-command.py
@@ -1,8 +1,11 @@
 import subprocess
+import os
 '''
 bashCommand = "sudo apt-get install openvpn; sudo openvpn --config ./config.ovpn"
 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, shell=True)
 output, error = process.communicate()
 '''
-subprocess.call(["sudo", "apt-get", "install", "openvpn"])
-subprocess.call(["sudo", "openvpn", "--config", "./config.ovpn"])
+
+# subprocess.call(["sudo", "apt-get", "install", "openvpn"])
+# subprocess.call(["sudo", "openvpn", "--config", "./config.ovpn"])
+os.system('start /wait cmd /k "cd C:\Program Files\OpenVPN\easy-rsa"')
