d625c7f437f1d8cd2e7d3461fce4ee0bc4315750
KeAndre96 <grahamkeandre@gmail.com>
2020-04-28 17:40:25 -0400

    updated open vpn command script

--- Backend/auth_server/open-vpn-command.py
+++ Backend/auth_server/open-vpn-command.py
@@ -1,11 +1,30 @@
 import subprocess
 import os
+# Some commands to install OpenVPN on Linux
 '''
 bashCommand = "sudo apt-get install openvpn; sudo openvpn --config ./config.ovpn"
 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, shell=True)
 output, error = process.communicate()
+
+subprocess.call(["sudo", "apt-get", "install", "openvpn"])
+subprocess.call(["sudo", "openvpn", "--config", "./config.ovpn"])
 '''
 
-# subprocess.call(["sudo", "apt-get", "install", "openvpn"])
-# subprocess.call(["sudo", "openvpn", "--config", "./config.ovpn"])
-os.system('start /wait cmd /k "cd C:\Program Files\OpenVPN\easy-rsa"')
+
+
+''' 
+Commands to generate certificate and keys 
+Must be in the OpenVPN\easy-rsa folder to run commands
+Must have the cmd in admin mode
+'''
+
+#os.system('start /wait cmd /k "cd C:\Program Files\OpenVPN\easy-rsa"')
+
+subprocess.call("init-config", shell=True)
+#subprocess.call("vars.bat", shell=True)
+subprocess.call("clean-all", shell=True)
+subprocess.call("build-dh", shell=True)
+subprocess.call("build-ca", shell=True)
+subprocess.call("build-key-server ServerVPN", shell=True)
+subprocess.call("build-key ClientVPN", shell=True)
+subprocess.call("openvpn --genkey --secret keys/ta.key", shell=True)
